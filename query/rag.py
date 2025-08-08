ebbb6488e8ef7343d116bfe62016b498a15c2a92
guptaautkarsh <utkarshgupta3125@gmail.com>
2025-08-09 02:08:03 +0530

    Added response query function

--- query/rag.py
+++ query/rag.py
@@ -7,9 +7,16 @@ load_dotenv()
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
-response = client.models.generate_content(
-  model="gemini-2.5-flash", 
-  contents="Explain how AI works in a few words"
-)
+# response = client.models.generate_content(
+#   model="gemini-2.5-flash",
+#   contents="Explain how AI works in a few words"
+# )
 
-print(response.text)
+def query_response(question):
+  response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=question
+  )
+  return response.text
+
+# print(response.text)
